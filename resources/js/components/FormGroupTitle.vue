7c34c0559334f7670cbdedf0141b43ea7c234a89
Gijs Mooren <info@app-en-vloed.com>
2023-10-17 23:05:51 +0200

    Add component for group title

---
+++ resources/js/components/FormGroupTitle.vue
@@ -0,0 +1,15 @@
+<template>
+    <p class="text-80 grow px-4">
+        <span class="mr-3 font-semibold">#{{ index + 1 }}</span>
+        {{ group.title }}
+    </p>
+</template>
+
+<script>
+export default {
+    props: {
+        index: null,
+        group: {}
+    }
+}
+</script>
